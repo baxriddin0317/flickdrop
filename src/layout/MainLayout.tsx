497f5d2a80f2a6e649c36bc9109d068a3fd0eace
baxriddin0317 <baxriddinxojaaxmadaliyev@gmail.com>
2025-11-26 17:14:58 +0500

    remove commit

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -13,15 +13,15 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      {/* <div className="w-full py-6 overflow-x-hidden relative z-10">
+      <div className="w-full py-6 overflow-x-hidden relative z-10">
         <WeaponSlider />
-      </div> */}
+      </div>
       <MobileMenu />
-      {/* <main className="mb-40">
+      <main className="mb-40">
         {children}
-      </main> */}
+      </main>
 
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
